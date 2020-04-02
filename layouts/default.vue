f752e05a7132a158f14f23b79a0a263bf666068c
sugasaki <sugasaki@gmail.com>
2020-04-02 23:09:18 +0900

    #79 メニューを変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -35,7 +35,7 @@
 import Vue from 'vue'
 import { MetaInfo } from 'vue-meta'
 import ScaleLoader from 'vue-spinner/src/ScaleLoader.vue'
-import SideNavigation from '@/components/SideNavigation.vue'
+import SideNavigation from '@/brigade/nagasaki/components/SideNavigation.vue'
 import NoScript from '@/components/NoScript.vue'
 import DevelopmentModeMark from '@/components/DevelopmentModeMark.vue'
 
