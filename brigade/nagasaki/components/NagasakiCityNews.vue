98c8a40e34bcd702cc8486d6cafb8a1d1fa0ee20
tkt989 <tkt989.dev@gmail.com>
2020-04-11 19:13:23 +0900

    長崎市からのお知らせのをトップページに表示するように変更

---
+++ brigade/nagasaki/components/NagasakiCityNews.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="NagasakiCityNews">
+    <h3 class="heading">
+      {{ $t('長崎市からのお知らせ') }}
+    </h3>
+    <ul class="list">
+      <li v-for="(item, i) in items" :key="i" class="item">
+        <a
+          class="anchor"
+          :href="item.url"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <time class="time px-2" :datetime="formattedDate(item.date)">
+            {{ formattedDateForDisplay(item.date) }}
+          </time>
+
+          <span class="link">
+            {{ item.text }}
+            <v-icon class="ExternalLinkIcon" size="12">
+              mdi-open-in-new
+            </v-icon>
+          </span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {
+  convertDateToISO8601Format,
+  convertDateByCountryPreferTimeFormat
+} from '@/utils/formatDate'
+
+export default Vue.extend({
+  computed: {
+    items() {
+      const news = this.$store.state.nagasakiCityNews
+      const result = news.slice().sort((a: any, b: any) => {
+        return b.date.getTime() - a.date.getTime()
+      })
+      return result
+    }
+  },
+  methods: {
+    formattedDate(dateString: string) {
+      return convertDateToISO8601Format(dateString)
+    },
+    formattedDateForDisplay(dateString: string) {
+      return convertDateByCountryPreferTimeFormat(dateString, this.$i18n.locale)
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.NagasakiCityNews {
+  @include card-container();
+
+  padding: 10px;
+  margin-bottom: 20px;
+
+  .heading {
+    display: flex;
+    align-items: center;
+
+    @include card-h2();
+
+    margin-bottom: 12px;
+    color: $gray-2;
+    margin-left: 12px;
+  }
+
+  .list {
+    padding-left: 0;
+    list-style-type: none;
+
+    .item {
+      .anchor {
+        text-decoration: none;
+        margin: 5px;
+        font-size: 14px;
+
+        @include lessThan($medium) {
+          display: flex;
+          flex-wrap: wrap;
+        }
+
+        .time {
+          flex: 0 0 90px;
+
+          @include lessThan($medium) {
+            flex: 0 0 100%;
+          }
+
+          color: $gray-1;
+        }
+
+        .link {
+          flex: 0 1 auto;
+
+          @include text-link();
+
+          @include lessThan($medium) {
+            padding-left: 8px;
+          }
+        }
+
+        .ExternalLinkIcon {
+          margin-left: 2px;
+          color: $gray-3 !important;
+        }
+      }
+    }
+  }
+}
+</style>
--- brigade/nagasaki/components/SideNavigation.vue
+++ brigade/nagasaki/components/SideNavigation.vue
@@ -197,10 +197,6 @@ export default Vue.extend({
             'https://www.city.nagasaki.lg.jp/shimin/121000/121100/p034298.html'
         },
         {
-          title: this.$t('長崎市からのお知らせ'),
-          link: this.localePath('/nagasaki/nagasaki-city-news')
-        },
-        {
           title: this.$t('当サイトについて'),
           link: this.localePath('/nagasaki/about')
         }
