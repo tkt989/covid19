c0ed60d2a81f031715489b95e4d36636109a8ada
atsushi sugasaki <sugasaki.atsushi@runners.co.jp>
2020-04-28 13:47:03 +0900

    #269 sleepの時間間違えてた

--- pages/index.vue
+++ pages/index.vue
@@ -99,7 +99,7 @@ export default Vue.extend({
   },
   async mounted() {
     // 動的に最新情報を取得する
-    await sleep(4000)
+    await sleep(50)
     await this.$store.dispatch('GET_BODIK_JSONP')
   },
   head(): MetaInfo {
